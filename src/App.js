9d6a0dfc3cd413173852054cfeee5dc6695d3ec6
Kurt Van Etten <kurt@vanetten.us>
2019-08-21 21:23:36 -0400

    Added broken code for timer :)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
 
+  // idx 0 => home, idx 1 => away
   const [scores, updateScores] = useState([0,0]);
   const [teamNames, updateNames] = useState(['Lions', 'Tigers']);
   const [inPossession, updateInPossession] = useState(0);
-  const [clock, updateClock] = useState('00:03');  // we'll do something better with this later
+  const [clock, updateClock] = useState([59,59]);  // minutes, seconds
 
   const changeScore = (team, amount) => {
     const [home, away] = scores;
@@ -21,6 +22,20 @@ function App() {
   const homeFG = () => changeScore(0, 3);
   const awayFG = () => changeScore(1, 3);
 
+
+/*
+  setInterval(() => {
+    let [minutes, seconds] = clock;
+    seconds--;
+    if (seconds < 0) {
+      seconds = 59;
+      minutes--;
+    }
+    updateClock([minutes, seconds]);
+  }, 1000);
+*/
+
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -29,7 +44,9 @@ function App() {
             <h2 className="home__name">{teamNames[0]}</h2>
             <div className="home__score">{scores[0]}</div>
           </div>
-          <div className="timer">{clock}</div>
+
+          <div className="timer">{`${clock[0]}:${clock[1]}`}</div>
+
           <div className="away">
             <h2 className="away__name">{teamNames[1]}</h2>
             <div className="away__score">{scores[1]}</div>
